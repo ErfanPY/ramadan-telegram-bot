7937f7a6fe2df435c75df9f9d10e01469b0fbc53
Erfan Ghofrani <parsyab1@gmail.com>
2021-04-23 13:53:37 +0430

    replace api, add logging

--- ramadan.py
+++ ramadan.py
@@ -3,25 +3,39 @@ import requests
 import datetime
 import time
 import pytz
+from enum import Enum
+import logging
 
-# A python bot to repost a telegram message that passed one month from first post
-# to reread the programming posts
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-def get_azan(city, day=None):
-    data = requests.get('https://api.keybit.ir/owghat', params={'city':city, "day": day})
-    json_data = json.loads(data.content)['result']
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
 
-    azan = json_data['azan_maghreb']
-    day  = json_data['day']
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
+
+class AzanTypes(Enum):
+    Maghrib = 'Maghrib'
+
+
+def get_azan(city, day=0, azan_type=AzanTypes.Maghrib):
+    url = f"http://api.aladhan.com/v1/timingsByAddress"
+    data = requests.get(url, params={"address":city, "method": 7, "adjustment": day})
+    json_data = json.loads(data.content)['data']
     
-    return azan, day
-
-cities_code = {'تهران': '1', 'مشهد': '13', 'تبریز': '6', 'قم': '11', 'اصفهان': '2', 'شیراز': '8', 'یزد': '14', 'ارومیه': '3', 'کرمان': '19', 'اهواز': '5', 'بجنورد': '15', 'گرگان': '18', 'ساری': '17', 'رشت': '16', 'اردبیل': '26', 'سمنان': '27', 'قزوین': '10', 'زنجان': '25', 'سنندج': '22', 'کرمانشاه': '21', 'همدان': '24', 'بیرجند': '827', 'ایلام': '29', 'خرم آباد': '30', 'شهرکرد': '31', 'یاسوج': '32', 'بوشهر': '20', 'زاهدان': '12', 'بندر عباس': '7', 'اراک': '4', 'کرج': '9', 'دزفول': '342', 'کوالالامپور': '115', 'ساوه': '991', 'شاهرود': '578', 'بروجرد': '479', 'مرند': '450', 'سبزوار': '822', 'استانبول': '976', 'کیش': '968'}
+    azan = json_data['timings'][azan_type.value]
+    
+    return azan
 
 def get_now():
     TEH = pytz.timezone('Asia/Tehran')
     now = datetime.datetime.now(TEH)
     now_text = str(now).split(' ')[-1].split('.')[0]
+    return "21:00:00"
     return now_text
 
 def delta(time1, time2, fix_hour):
@@ -42,18 +56,20 @@ def delta(time1, time2, fix_hour):
         
     return (h, m, s)
 
-#EdgeCase: Last day of month if person ask after azan
-
-def calculate_reminder(city, azan_day=None):
-    azan, day = get_azan(city, azan_day)
+def calculate_reminder(city, azan_day=0):
+    azan = get_azan(city, azan_day)+":00"
     now  = get_now()
+    
     rH, rM, rS = delta(now, azan, bool(azan_day))
+
+    logger.debug(f"Now: {now} Azan is: {azan} >> {rH}:{rM}:{rS}")
     if rH < 0:
-        rH, rM, rS = calculate_reminder(day+1)
+        rH, rM, rS = calculate_reminder(city, 1)
     return rH, rM, rS
 
 if __name__ == "__main__":
     city = "بيرجند"
+    ch.setLevel(logging.DEBUG)
     print(calculate_reminder(city))
 
 """
